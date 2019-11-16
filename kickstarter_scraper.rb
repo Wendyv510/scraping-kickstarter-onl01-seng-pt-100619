add32a30c63e65e75b107dc73bd0d593248e733f
Wendy Harris <wendyv510@hotmail.com>
2019-11-16 15:53:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,9 +5,10 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
      kickstarter = Nokogiri::HTML(html)
-     binding.pry
   
-end
+  projects = {} 
+  
+  
 
 create_project_hash
 
@@ -16,4 +17,4 @@ create_project_hash
 #image link: project.css("div.project-thumbnail a img").attribute("src").value
 #description: project.css("p.bbcard_blurb").text
 #location: project.css("span.location-name").text
-#percent_funded: 
+#percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i 
